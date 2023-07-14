95a2ac7f38af51a58ed6a2134f3d129f817c3b55
muni <booscarface@gmail.com>
2023-07-14 18:01:23 +0200

    form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import moment from 'moment'
+import { SingleDatePicker } from 'react-dates'
 
 
 const ExpenseForm = (props) => {
@@ -16,9 +17,71 @@ const ExpenseForm = (props) => {
         setDescription({description})
     }
 
+    const handleNoteChange = (e) => {
+        const note = e.target.value
+        setNote({note})
+    }
+    
+    const handleAmountChange = (e) => {
+        const amount = e.target.value
+        if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+            this.setState({amount})
+        }
+    }
+
+    const handleDateChange = (createdAt) => {
+        if(createdAt) {
+            setCreatedAt({createdAt})
+        }
+    }
+
+    const handleFocusChange = ({focused}) => {
+        setCalendardFocused(focused)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if(!description || !amount) {
+            setError('Please provide the description and the amount')
+        }
+        setError('')
+        setDescription(description)
+        setAmount(amount)
+        setCreatedAt(createdAt)
+        setNote(note)
+}
     return (
         <div>
-            <h1>Add Expense</h1>
+            {error && <p>{error}</p>}
+            <form onSubmit={onSubmit}>
+                <input
+                    type='text'
+                    placeholder='Description'
+                    autoFocus
+                    value={description}
+                    onChange={handleDescriptionChange}
+                />
+                <input
+                    type='text'
+                    placeholder='Amount'
+                    value={amount}
+                    onChange={handleAmountChange}
+                />
+                <SingleDatePicker
+                    date={createdAt}
+                    onDateChange={handleDateChange}
+                    focused={calendarFocused}
+                    onFocusChange={handleFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                />
+                <textarea>
+                    placeholder="Add a note for your expense (optimal)"
+                    value={note}
+                    onChange={handleNoteChange}
+                </textarea>
+                <button>Add Expense</button>
+            </form>
         </div>
     )
 }
