aeaff0d1ab785497ac4b007682cf97037add7be0
muni <booscarface@gmail.com>
2023-07-14 20:29:42 +0200

    reducers

--- src/store/configurationStore.js
+++ src/store/configurationStore.js
@@ -1,1 +1,11 @@
-import { configureStore, combineReducers} from 'redux'
+import { createStore, combineReducers} from 'redux'
+
+
+export default () => {
+    const store = createStore(
+        combineReducers(),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    return store
+}
+    
